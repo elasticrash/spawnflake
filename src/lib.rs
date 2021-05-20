d03a212415b09a6f9977f9621ebf5ef9e3a708db
stefano <s.kouroupis@gmail.com>
2021-05-20 01:28:20 +0100

    update documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
+/// This module allows you to configure your generators
+/// * The optional reader
+/// * The model for setting up your project configuration
 pub mod configuration;
+
+/// Database functionality
 pub mod databases;
+/// generates name based on patterns
 pub mod name_generator;
+
+/// generates random numbers
 pub mod number_generator;
+
+/// generates random strings
 pub mod string_generator;
