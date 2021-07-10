09539596297e87e3bb04aaf30cae0b015e3fc2a9
stefano <s.kouroupis@gmail.com>
2021-07-11 00:31:27 +0100

    working version of the cli

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,17 +1,70 @@
-use std::env;
-use spawnflake::{
-    configuration::{self, config_model::GenericConfiguration},
-    databases::my_sql::spawn::spawn,
-};
+use clap::{App, Arg};
+use spawnflake::{configuration::{self, config_model::GenericConfiguration}, datastores::{datastore::DataGeneration, my_sql::datastore_models::Mysql, postgres::datastore_models::Postgres}};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let mut items:i32 = 100;
+    let matches = App::new("Spawnflake CLI")
+    .version("0.2.0")
+    .author("Stefanos Kouroupis. <manekato@gmail.com>")
+    .about("Generates random data")
+    .arg(Arg::with_name("spawn-size")
+         .short("s")
+         .long("spawn-size")
+         .help("Set the number of rows per table to be generated")
+         .takes_value(true))
+    .arg(Arg::with_name("datastore")
+         .short("d")
+         .help("Sets the type of the datastore to generate values. By default executes the entire configuration")
+         .takes_value(true))
+    .arg(Arg::with_name("configuration")
+         .short("c")
+         .help("sets a custom configuration name and path relative to this location")
+         .takes_value(true))
+    .get_matches();
 
-    if args.len() > 1 {
-        items = args[1].parse().unwrap();
-    }
     
-    let config: GenericConfiguration = configuration::reader::read("./config.json").unwrap();
-    spawn(&config, items);
+    let configuration_file: &str = matches
+    .value_of("configuration")
+    .unwrap_or("./config.json");
+
+    let config: GenericConfiguration = configuration::reader::read(configuration_file).unwrap();
+    let size: i32 = matches
+        .value_of("spawn-size")
+        .unwrap_or("100")
+        .parse()
+        .unwrap();
+    let datastore: &str = matches
+        .value_of("datastore")
+        .unwrap_or("all");
+
+
+    if datastore == "all" {
+        if config.mysql_configuration != None {
+            println!("generating data for mysql datastore");
+            let mut db = Mysql::new();
+            db.spawn(&config, size);
+        }
+        if config.postgres_configuration != None {
+            println!("generating data for postgres datastore");
+            let mut db = Postgres::new();
+            db.spawn(&config, 10);
+        }
+    }
+
+    if datastore  == "mysql"{
+        if config.mysql_configuration != None {
+            let mut db = Mysql::new();
+            db.spawn(&config, size);
+        } else{
+            println!("mysql Configuration is missing");
+        }
+    }
+
+    if datastore  == "postgres"{
+        if config.postgres_configuration != None {
+            let mut db = Postgres::new();
+            db.spawn(&config, 10);
+        } else{
+            println!("postgres Configuration is missing");
+        }
+    }
 }
